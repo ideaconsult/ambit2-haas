46dea1001d765420c065954e722e89fbab663e9f
vedina <jeliazkova.nina@gmail.com>
2018-06-10 14:47:02 +0300

    added test with real exnet soap service

--- ambit2-haas/src/test/java/net/idea/ambit/algorithm/test/AlgorithmHaasTest.java
+++ ambit2-haas/src/test/java/net/idea/ambit/algorithm/test/AlgorithmHaasTest.java
@@ -17,13 +17,27 @@ public class AlgorithmHaasTest extends ResourceTest {
 	}
 	
 	@Test
-	public void testExnet() throws Exception {
+	public void testExnetMockup() throws Exception {
 		Form webform = new Form();
 		webform.add("learning_rate_list", "1.0");
 		webform.add("network_layout_list", "200,200");
 		webform.add("network_layout_list", "2000,2000");
 		webform.add("network_layout_list", "");
 		Reference url = testAsyncTask(getTestURI(),
+				webform, Status.SUCCESS_OK, null);
+		String expected = String.format("http://localhost:%d/model/0", port);
+		Assert.assertEquals(expected, url.toString());
+
+	}
+	
+	@Test
+	public void testExnet() throws Exception {
+		Form webform = new Form();
+		webform.add("learning_rate_list", "0.5");
+		webform.add("network_layout_list", "2048,2048");
+		String exneturi = String.format("http://localhost:%d%s/haasexnet", port,
+				MLResources.algorithm);
+		Reference url = testAsyncTask(exneturi,
 				webform, Status.SUCCESS_OK, null);
 		String expected = String.format("http://localhost:%d/model/0", port);
 		Assert.assertEquals(expected, url.toString());
