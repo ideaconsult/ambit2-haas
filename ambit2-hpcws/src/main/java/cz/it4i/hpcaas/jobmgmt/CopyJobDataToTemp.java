9fc2cbb45eb5d959c5e598a211038b7f3c4be4f1
vedina <jeliazkova.nina@gmail.com>
2018-06-12 11:27:30 +0300

    new heappe classes

---
+++ ambit2-hpcws/src/main/java/cz/it4i/hpcaas/jobmgmt/CopyJobDataToTemp.java
@@ -0,0 +1,108 @@
+
+package cz.it4i.hpcaas.jobmgmt;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="submittedJobInfoId" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="sessionCode" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="path" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "submittedJobInfoId",
+    "sessionCode",
+    "path"
+})
+@XmlRootElement(name = "CopyJobDataToTemp")
+public class CopyJobDataToTemp {
+
+    protected long submittedJobInfoId;
+    protected String sessionCode;
+    protected String path;
+
+    /**
+     * Gets the value of the submittedJobInfoId property.
+     * 
+     */
+    public long getSubmittedJobInfoId() {
+        return submittedJobInfoId;
+    }
+
+    /**
+     * Sets the value of the submittedJobInfoId property.
+     * 
+     */
+    public void setSubmittedJobInfoId(long value) {
+        this.submittedJobInfoId = value;
+    }
+
+    /**
+     * Gets the value of the sessionCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSessionCode() {
+        return sessionCode;
+    }
+
+    /**
+     * Sets the value of the sessionCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSessionCode(String value) {
+        this.sessionCode = value;
+    }
+
+    /**
+     * Gets the value of the path property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Sets the value of the path property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPath(String value) {
+        this.path = value;
+    }
+
+}
